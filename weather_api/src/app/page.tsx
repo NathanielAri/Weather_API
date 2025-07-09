e8885752c954208d9644a3a479a853f36bf5b709
Nate505 <nathanielarifin@gmail.com>
2025-07-08 21:58:15 -0700

    Weather API

--- weather_api/src/app/page.tsx
+++ weather_api/src/app/page.tsx
@@ -1,103 +1,84 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import { useState } from "react"
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+type WeatherData = {
+    main: {
+        temp: number
+    }
+    weather: {description: string}[]
+    locationName: string
+    country: string
+}
+
+export default function Page(){
+    const [city, setCity] = useState('')
+    const [weather, setWeather] = useState<WeatherData | null>(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+
+    const fetchWeather = async () => {
+        if(!city) return setError('Please enter a city name')
+
+        setLoading(true)
+        setError(null)
+
+        try {
+            const apiKey = '7dd68e758436a503914212743304a446'
+            const geoRes = await fetch(
+                `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=1&appid=${apiKey}`
+            )
+
+            const geoData = await geoRes.json()
+
+            if(!geoRes.ok || !geoData.length) throw new Error('City not Found')
+
+            const { lat, lon, name, country } = geoData[0]
+
+            const weatherRes = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+            )
+
+            const weatherData = await weatherRes.json()
+
+            if(!weatherRes.ok) throw new Error(weatherData.message)
+
+            weatherData.locationName = name
+            weatherData.country = country
+
+            setWeather(weatherData)
+        } catch(err: any){
+            setError(err.message)
+            setWeather(null)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return(
+        <div>
+            <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city name"/>
+            <button onClick={fetchWeather}>
+                Get Weather
+            </button>
+
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            {weather && (
+                <div>
+                    <h2>
+                        {weather.locationName}, {weather.country} Weather
+                    </h2>
+                    <p>
+                        Temperature: {weather.main.temp}
+                    </p>
+                    <p>
+                        Weather: {weather.weather[0].description}
+                    </p>
+                </div>
+            )}
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
+
 }
